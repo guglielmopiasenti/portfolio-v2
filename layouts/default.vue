d016e046ce14fbcd91cec9ad98cf9931336a0272
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-11-13 16:39:46 +0100

    header

---
+++ layouts/default.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref } from 'vue'
+import {
+  Dialog,
+  DialogPanel,
+  PopoverGroup,
+} from '@headlessui/vue'
+import {
+  Bars3Icon,
+  XMarkIcon,
+} from '@heroicons/vue/24/outline'
+const mobileMenuOpen = ref(false)
+</script>
+
+<template>
+  <header class="bg-black sticky top-0 z-50 glass-navbar backdrop-blur-sm bg-opacity-60">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <!-- <img class="h-12 w-auto rounded-full" src="public/img/ai_murazzi.png" alt="" /> -->
+        </a>
+      </div>
+      <div class="flex lg:hidden">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true">
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <PopoverGroup class="hidden lg:flex lg:gap-x-12">
+
+        <!-- routes -->
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/about">About Me</NuxtLink>
+        <NuxtLink to="/contact">Contact Me</NuxtLink>
+
+
+      </PopoverGroup>
+    </nav>
+    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black glass-navbar backdrop-blur-sm bg-opacity-60 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img class="h-12 w-auto rounded-full" src="public/img/ai_murazzi.png" alt="" />
+          </a>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-20 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-10 grid grid-cols-1 gap-y-7">
+              <NuxtLink class="text-2xl font-bold leading-6 text-white" to="">
+                Home
+              </NuxtLink>
+              <NuxtLink class="text-2xl font-bold leading-6 text-white" to="">
+                About Me
+              </NuxtLink>
+              <NuxtLink class="text-2xl font-bold leading-6 text-white" to="">
+                Contact Me
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
+</template>
