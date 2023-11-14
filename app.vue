600bc0f34331a74021d0b0068a54236545a056aa
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-11-14 16:47:22 +0100

    title chunk

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunck) => {
+    return titleChunck ? `${titleChunck} - Guglielmo Piasenti` : 'Guglielmo Piasenti'
+  }
+})
+</script>
+
+
 <template>
-  <div>
+  <div class="bg-black text-white">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
