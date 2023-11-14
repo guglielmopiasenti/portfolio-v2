9381f33f213570622f5200890e423b1ae4e91c11
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-11-14 15:05:28 +0100

    index bare bones

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
 <script setup>
-
+useHead({
+  title: "Home"
+})
 </script>
 
 <template>
   <div>
-    Page: foo
+    <div class="container grid grid-cols-2 min-h-screen mx-auto py-5">
+      <div class="border-2 border-green-600">
+        <h1>Hello, I'm Guglielmo.</h1>
+        <h1>A Full Stack Developer.</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto nihil cumque hic est? Libero quae nihil est
+          eaque magni officia quam voluptas asperiores dolorem quis? Sint perspiciatis est tempora enim.</p>
+        <div>
+          <div>you can find me on:</div>
+          <div></div>
+        </div>
+      </div>
+      <div class="border-2 border-blue-600">
+        projects
+      </div>
+
+    </div>
   </div>
 </template>
 
