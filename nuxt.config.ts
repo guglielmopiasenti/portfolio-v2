bb45d28b264d2245e45100d014b7fc788c393d9c
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-11-24 15:40:13 +0100

    reupload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
   css: ['~/assets/css/main.css'],
+  supabase: {
+    redirect: false,
+  },
 })
