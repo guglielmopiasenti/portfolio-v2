bf0ed60e35e724df85d6bc2241573223b1255a7d
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-11-28 09:22:06 +0100

    font awesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/supabase",
+    "@vueuse/nuxt",
+  ],
   css: ['~/assets/css/main.css'],
   supabase: {
     redirect: false,
