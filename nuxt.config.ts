cecf484a4330b2cbf1a01348ff212d21dc509365
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-04 19:27:31 +0100

    adding supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
   supabase: {
     redirect: false,
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
   },
 })
