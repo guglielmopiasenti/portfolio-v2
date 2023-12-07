a2cb396df971cb6ecd2db8e6d1183f98f409dadf
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-12-07 15:38:33 +0100

    container structure

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,4 +1,23 @@
-<script setup>
-</script>
+<script setup></script>
 
-<template></template>
+<template>
+  <div class="antialiased">
+    <section class="hero">
+      <div class="container">
+        <h1></h1>
+      </div>
+    </section>
+    <main>
+      <!-- Company description -->
+      <section></section>
+      <!-- Carousel -->
+      <section></section>
+      <!-- project description -->
+      <section></section>
+      <!-- mockup -->
+      <section></section>
+      <!-- next/previous project -->
+      <div></div>
+    </main>
+  </div>
+</template>
