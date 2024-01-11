69ae2e8a0a900403fb06f8930e9cacb29a9ebb21
guglielmopiasenti <piasentiguglielmo@gmail.com>
2024-01-11 20:45:17 +0100

    header fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,7 @@ const mobileMenuOpen = ref(false)
 
 <template>
   <header class="bg-black absolute top-0 left-0 right-0 z-50 glass-navbar backdrop-blur bg-opacity-30 antialiased">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <nav class="mx-auto flex max-w-[1600px] items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
           <h1 class="font-extrabold text-2xl">Guglielmo P.</h1>
